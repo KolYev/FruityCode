d155821379d2752567df73af812019c78fe97450
KolYev <evgeniy.kolesenko@gmail.com>
2025-08-23 21:31:00 +0300

    first commit

---
+++ source/javascript/login.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const togglePassword = document.getElementById('toggle-password');
+    const passwordField = document.getElementById('password');
+
+    togglePassword.addEventListener('click', function () {
+        const type = passwordField.getAttribute('type') === 'password' ? 'text' : 'password';
+        passwordField.setAttribute('type', type);
+
+        this.classList.toggle('view');
+    });
+});
